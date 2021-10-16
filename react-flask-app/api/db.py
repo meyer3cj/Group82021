2333404a624cf4c2c8864bd424fc45e2f831ee82
meyer3cj <meyer3cj@mail.uc.edu>
2021-10-16 15:03:19 -0400

    Implement database connection

---
+++ react-flask-app/api/db.py
@@ -0,0 +1,38 @@
+import pyodbc
+import textwrap
+
+driver='{ODBC Driver 17 for SQL Server}'
+
+server_name='kartzserver'
+database_name='KartzDB'
+server='{server_name}.database.windows.net,1433'.format(server_name=server_name)
+
+username= "admin5000"
+password= "Pw^?CWu']y}@Ym9g"
+
+connectionString= textwrap.dedent('''
+    Driver={driver};
+    Server={server};
+    Database={database};
+    Uid={username};
+    Pwd={password};
+    Encrypt=yes;
+    Connection Timeout=30;
+
+
+'''.format(
+    driver=driver,
+    server=server,
+    database=database_name,
+    username=username,
+    password=password
+
+))
+cnxn: pyodbc.Connection= pyodbc.connect(connectionString)
+
+csr: pyodbc.Cursor= cnxn.cursor()
+
+select= "SELECT * FROM [TEST]"
+csr.execute(select)
+print(csr.fetchall())
+            
