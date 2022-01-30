3be28cf98173f38431f00253a9b11511310165d8
Alex Mezcua <mezcuaaj@mail.uc.edu>
2022-01-29 20:06:14 -0500

    Working changes after rebase.

--- react-flask-app/src/Views/Login.js
+++ react-flask-app/src/Views/Login.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Form, Input, Button } from 'semantic-ui-react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import "../CSS_Files/loginStyle.css";
 
 // TODO: This is used in many places, can extract into its own file
 const api = axios.create({
@@ -32,57 +33,101 @@ const Login = () => {
         navigate(`/signup`);
     }
 
-    return(   
-        <Form >
-            <Form.Field>
-                <Input 
-                    value = {username}
-                    onChange = {e => setUsername(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Input 
-                    value = {password}
-                    onChange = {e => setPassword(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Button className = "submitButton" onClick={async (e) => {
-                    e.preventDefault();
+    return(
+        <div className='container'>
+            <div className='forms-container'>
+                <div className='signin-signup'>
+                    <Form 
+                        action='#' 
+                        className='sign-in-form'
+                    >
+                        <h2 className='title'>Sign in</h2>
+                        <Form.Field className='input-field'>
+                            <i className='fas fa-user' />
+                                <Input 
+                                    placeholder = "Username"
+                                    value = {username}
+                                    onChange = {e => setUsername(e.target.value)}
+                                />
+                        </Form.Field>
+                        <Form.Field className='input-field'>
+                            <i className='fas fa-user' />
+                                <Input 
+                                    placeholder = "Password"
+                                    value = {password}
+                                    onChange = {e => setPassword(e.target.value)}
+                                />
+                        </Form.Field>
+                        <Form.Field>
+                            <Button className = 'btn solid' onClick={async (e) => {
+                                e.preventDefault();
 
-                    // Wrap inside of try catch
-                    const credenitals = {
-                        username: username,
-                        password: password
-                    };
-                    
-                    try {
-                        // Send username and password to the server.
-                        const response = await api.post(`/login`, credenitals)
+                                // Wrap inside of try catch
+                                const credenitals = {
+                                    username: username,
+                                    password: password
+                                };
+                                
+                                try {
+                                    // Send username and password to the server.
+                                    const response = await api.post(`/login`, credenitals)
 
-                        // Store the user in localStorage
-                        localStorage.setItem('user', JSON.stringify(response.data))
+                                    // Store the user in localStorage
+                                    localStorage.setItem('user', JSON.stringify(response.data))
 
-                        // Get stored data to navigate
-                        const user = JSON.parse(localStorage.getItem("user"));
+                                    // Get stored data to navigate
+                                    const user = JSON.parse(localStorage.getItem("user"));
 
-                        if (response.status === 200) {
-                            navigate(`/home/${user[0].userId}`)
-                        }
-                    }
-                    catch {
-                        // TODO: Look into a possible better way to check status
-                        // Unauthorized so need to change page.
-                    }
-                    
-                }}>
-                    Submit
-                </Button>
-                <Button onClick={navigateSignup}>
-                    Signup
-                </Button>
-            </Form.Field>
-        </Form>
+                                    if (response.status === 200) {
+                                        navigate(`/home/${user[0].userId}`)
+                                    }
+                                }
+                                catch {
+                                    // TODO: Look into a possible better way to check status
+                                    // Unauthorized so need to change page.
+                                }
+                                
+                            }}>
+                                Submit
+                            </Button>
+                            <p className='social-text'>Or Sign in with social platforms</p>
+                            <div className='social-media'>
+                                <a href="#" className='social-icon'>
+                                    <i className='fab fa-facebook-f'></i>
+                                </a>
+                                <a href="#" className='social-icon'>
+                                    <i className='fab fa-twitter'></i>
+                                </a>
+                                <a href="#" className='social-icon'>
+                                    <i className='fab fa-google'></i>
+                                </a>
+                                <a href="#" className='social-icon'>
+                                    <i className='fab fa-linkedin-in'></i>
+                                </a>
+                            </div>
+                        </Form.Field>
+                    </Form>
+                </div>
+            </div>
+
+            <div className='panels-container'>
+                <div className='panel left-panel'>
+                    <div className='content'>
+                        <h3>New Here?</h3>
+                        <p>
+                            The site is designed to allow a user to gather items from their online shopping carts and can notify them of stock updates and price changes from multiple sites. The application will also save purchase information for ease of access when completing a purchase. The site is aimed towards consumers that frequently shop online on multiple websites in order to organize and monitor online items and services.
+                        </p>
+                        <button
+                            className='btn transparent'
+                            id='sign-up-btn'
+                            onClick={navigateSignup}
+                        >
+                                Sign Up
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )   
 }
 
--- react-flask-app/src/Views/Signup.js
+++ react-flask-app/src/Views/Signup.js
@@ -31,125 +31,166 @@ const Signup = () => {
     }
 
     return(
-        <Form>
-            <Form.Field>
-                <Input
-                    className='input'
-                    type = "text"
-                    placeholder = "First Name"
-                    value = {firstName}
-                    onChange = {e => setFirstName(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Input
-                    className='input'
-                    type = "text"
-                    placeholder = "Last Name"
-                    value = {lastName}
-                    onChange = {e => setLastName(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Input
-                    className='input'
-                    type = "text"
-                    placeholder = "Password"
-                    value = {password}
-                    onChange = {e => setPassword(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Input
-                    className='input'
-                    type = "text"
-                    placeholder = "Date of Birth"
-                    value = {dob}
-                    onChange = {e => setDOB(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Input
-                    className='input'
-                    type = "text"
-                    placeholder = "Email"
-                    value = {email}
-                    onChange = {e => setEmail(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Input
-                    className='input'
-                    type = "text"
-                    placeholder = "City"
-                    value = {city}
-                    onChange = {e => setCity(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Input
-                    className='input'
-                    type = "text"
-                    placeholder = "State"
-                    value = {state}
-                    onChange = {e => setState(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Input
-                    className='input'
-                    type = "text"
-                    placeholder = "Zipcode"
-                    value = {zipCode}
-                    onChange = {e => setZipCode(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Input
-                    className='input'
-                    type = "text"
-                    placeholder = "Cell Phone"
-                    value = {cellPhone}
-                    onChange = {e => setCellPhone(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Input
-                    className='input'
-                    type = "text"
-                    placeholder = "Home Phone"
-                    value = {homePhone}
-                    onChange = {e => setHomePhone(e.target.value)}
-                />
-            </Form.Field>
-            <Form.Field>
-                <Button onClick={navigateLogin}>
-                    Cancel
-                </Button>
-                <Button className = "submitButton" onClick={async () => {
-                    const user = {
-                        firstName: firstName,
-                        lastName: lastName,
-                        password: password,
-                        dob: dob,
-                        email: email,
-                        city: city,
-                        state: state,
-                        zipCode: zipCode,
-                        cellPhone: cellPhone,
-                        homePhone: homePhone
-                    };
+        <div className='container'>
+            <div className='forms-container'>
+                <div className='signin-signup'>
+                    <Form 
+                        action='#'
+                    >
+                        <h2 className='title'>Sign up</h2>
+                        <Form.Field className='input-field'>
+                            <i className='fas fa-envelope' />
+                            <Input
+                                type = "text"
+                                placeholder = "Email"
+                                value = {email}
+                                onChange = {e => setEmail(e.target.value)}
+                            />
+                        </Form.Field>
+                        <Form.Field className='input-field'>
+                            <i className='fas fa-envelope' />
+                            <Input
+                                type = "text"
+                                placeholder = "Password"
+                                value = {password}
+                                onChange = {e => setPassword(e.target.value)}
+                            />
+                        </Form.Field>
 
-                    const response = await api.post('/signup', user)
+                        {/* <Form.Field className='input-field'>
+                            <i className='fas fa-user'/>
+                            <Input
+                                type = "text"
+                                placeholder = "First Name"
+                                value = {firstName}
+                                onChange = {e => setFirstName(e.target.value)}
+                            />
+                        </Form.Field>
+                            <Form.Field>
+                            <Input
+                                className='input'
+                                type = "text"
+                                placeholder = "Date of Birth"
+                                value = {dob}
+                                onChange = {e => setDOB(e.target.value)}
+                            />
+                        </Form.Field>
+                        <Form.Field>
+                            <Input
+                                className='input'
+                                type = "text"
+                                placeholder = "Email"
+                                value = {email}
+                                onChange = {e => setEmail(e.target.value)}
+                            />
+                        </Form.Field>
+                        <Form.Field>
+                            <Input
+                                className='input'
+                                type = "text"
+                                placeholder = "City"
+                                value = {city}
+                                onChange = {e => setCity(e.target.value)}
+                            />
+                        </Form.Field>
+                        <Form.Field>
+                            <Input
+                                className='input'
+                                type = "text"
+                                placeholder = "State"
+                                value = {state}
+                                onChange = {e => setState(e.target.value)}
+                            />
+                        </Form.Field>
+                        <Form.Field>
+                            <Input
+                                className='input'
+                                type = "text"
+                                placeholder = "Zipcode"
+                                value = {zipCode}
+                                onChange = {e => setZipCode(e.target.value)}
+                            />
+                        </Form.Field>
+                        <Form.Field>
+                            <Input
+                                className='input'
+                                type = "text"
+                                placeholder = "Cell Phone"
+                                value = {cellPhone}
+                                onChange = {e => setCellPhone(e.target.value)}
+                            />
+                        </Form.Field>
+                        <Form.Field>
+                            <Input
+                                className='input'
+                                type = "text"
+                                placeholder = "Home Phone"
+                                value = {homePhone}
+                                onChange = {e => setHomePhone(e.target.value)}
+                            />
+                        </Form.Field> */}
+                        <Form.Field>
+                            <Button onClick={navigateLogin}>
+                                Cancel
+                            </Button>
+                            <Button 
+                                type = 'submit'
+                                className = "btn" 
+                                onClick={async () => {
+                                const user = {
+                                    firstName: firstName,
+                                    lastName: lastName,
+                                    password: password,
+                                    dob: dob,
+                                    email: email,
+                                    city: city,
+                                    state: state,
+                                    zipCode: zipCode,
+                                    cellPhone: cellPhone,
+                                    homePhone: homePhone
+                                };
 
-                    if (response.status === 200) {
-                        navigate(`/login`);
-                    }
-                }}>
-                    Signup
-                </Button>
-            </Form.Field>
-        </Form>
+                                const response = await api.post('/signup', user)
+
+                                if (response.status === 200) {
+                                    navigate(`/login`);
+                                }
+                            }}>
+                                Signup
+                            </Button>
+                        </Form.Field>
+                        <div className='social-media'>
+                            <a href="#" className='social-icon'>
+                                <i className='fab fa-facebook-f'></i>
+                            </a>
+                            <a href="#" className='social-icon'>
+                                <i className='fab fa-twitter'></i>
+                            </a>
+                            <a href="#" className='social-icon'>
+                                <i className='fab fa-google'></i>
+                            </a>
+                            <a href="#" className='social-icon'>
+                                <i className='fab fa-linkedin-in'></i>
+                            </a>
+                        </div>
+                    </Form>
+                </div>
+            </div>
+            <div className='panel right-panel'>
+                    <div className='content'>
+                        <h3>One of us?</h3>
+                        <p>
+                        As a frequent online shopping user, I want to organize and save all desired products in one location, so I can keep track of which purchases I have to make or have made. As a company owner, I want a way to make multiple shopping lists in one application, so that I can order each of my employees required equipment. As a mother, I want a way to save gift ideas for my children for later date in a single location, so that I can revisit the items later. As a student, I want a way to order supplies needed for school at the most affordable rate from different sites, so I can get save the most money ordering my supplies. As a teacher, I want a way to save assignment or project ideas for my students that I could come back to later, so I can easily locate the items to order for those projects later.
+                        </p>
+                        <button 
+                            className='btn transparent' 
+                            id='sign-in-btn'
+                        >
+                            Sign In
+                        </button>
+                    </div>
+            </div>
+        </div>
     )
 }
 
